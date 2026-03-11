8ddb366b9399769855a166f2e4cd970a8d10fa34
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2026-03-11 19:03:13 +0530

    ignore file added

--- main.tf
+++ main.tf
@@ -1,99 +1,99 @@
-provider "aws"{
-    region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
 resource "aws_vpc" "my_vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-        key = "Name"
-        value = "my_vpc"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    key   = "Name"
+    value = "my_vpc"
+  }
 }
 
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.my_vpc.id
-    tags = {
-        key = "Name"
-        value = "my_igw_fe"
-    }
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    key   = "Name"
+    value = "my_igw_fe"
+  }
 }
 resource "aws_eip" "eip" {
-    domain = "vpc"
-    tags = {
-        key = "Name"
-        value = "VPC_EIP"
-    }
+  domain = "vpc"
+  tags = {
+    key   = "Name"
+    value = "VPC_EIP"
+  }
 }
 resource "aws_nat_gateway" "nat_gw" {
-    subnet_id = aws_subnet.public_subnet.id
-    allocation_id = aws_eip.eip.id
-    tags = {
-        key = "Name"
-        value = "nat_gw_be"
-    }
-    depends_on = [aws_internet_gateway.igw]
+  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.eip.id
+  tags = {
+    key   = "Name"
+    value = "nat_gw_be"
+  }
+  depends_on = [aws_internet_gateway.igw]
 }
 
 
 
 
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = true
-    tags = {
-        key = "Name"
-        value = "my_public_subnet"
-    }
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    key   = "Name"
+    value = "my_public_subnet"
+  }
 }
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.my_vpc.id
-    tags = {
-        key = "Name"
-        value = "my_public_route_table"
-    }
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    key   = "Name"
+    value = "my_public_route_table"
+  }
 }
 resource "aws_route" "public_route" {
-    route_table_id = aws_route_table.public_rt.id
-    gateway_id = aws_internet_gateway.igw.id
-    destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public_rt.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
 
 }
 resource "aws_route_table_association" "public_rta" {
-    subnet_id = aws_subnet.public_subnet.id
-    route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 
 
 
 resource "aws_subnet" "private_subnet" {
-    vpc_id = aws_vpc.my_vpc.id
-    availability_zone = "ap-south-1b"
-    cidr_block = "10.0.2.0/24"
-    tags = {
-        key = "Name"
-        value = "my_private_subnet"
-    }
+  vpc_id            = aws_vpc.my_vpc.id
+  availability_zone = "ap-south-1b"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    key   = "Name"
+    value = "my_private_subnet"
+  }
 }
 resource "aws_route_table" "private_rt" {
-    vpc_id = aws_vpc.my_vpc.id
-    tags ={
-        key = "Name" 
-        value = "my_private_route_table"
-    }
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    key   = "Name"
+    value = "my_private_route_table"
+  }
 }
 resource "aws_route" "private_route" {
-    route_table_id = aws_route_table.private_rt.id
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-    destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.private_rt.id
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route_table_association" "private_rta" {
-    subnet_id = aws_subnet.private_subnet.id
-    route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
