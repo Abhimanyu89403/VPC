cd3517fdde70ce7c6d324d8b97c1f605a3e37ad9
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2026-03-12 09:46:20 +0530

    backend and provider block added

---
+++ s3.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = "abhi-mystatebucket-vpc-backend-state-file"
+
+    tags = {
+        owner = "Abhimanyu"
+        purpose = "vpc_state_file"
+    }
+}
+resource "aws_s3_bucket_policy" "bucket_policy" {
+    bucket = aws_s3_bucket.my_bucket.id
+    policy = jsonencode ([
+        {
+            Sid = "Terraform_state_share"
+            Effect = "Allow"
+            Principal = {
+                AWS = [
+                    "arn:aws:iam::396608811643:user/Manyu89403"
+                ]
+            }
+            Action =[
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:ListObject"
+            ]
+        }
+    ])
+}
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+    bucket = aws_s3_bucket.my_bucket.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption" "bucket_encryption" {
+    bucket = aws_s3_bucket.my_bucket.id
+
+    rule {
+        apply_server_side_encryption {
+            sse_algorithm = "AES256"
+        }
+    }
+}
